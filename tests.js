1e8c898ed4a9dd1e1bd453d3c7d51d0797d8c46c
Diogo Kollross <diogoko@gmail.com>
2015-07-18 19:27:42 -0300

    Improved tests

--- tests.js
+++ tests.js
@@ -1,3 +1,7 @@
+// TODO: test other creation parameters
+// TODO: review and test all possible responses
+
+
 function makeServerTest(doRequest, assertions) {
   return function(assert) {
     var testDone = assert.async();
@@ -12,11 +16,15 @@ function makeServerTest(doRequest, assertions) {
 }
 
 
+var RESPONSE_HEADERS = { 'Content-Type': 'application/json' };
+
+
 QUnit.test('create (generated name)', makeServerTest(
   function(assert, testDone) {
     AppletServerClient.start({
       code: 'my.sample.Applet'
     }).then(function(applet) {
+      assert.equal(applet.name, 'generated-name', 'the generated name was received');
       testDone();
     });
   },
@@ -24,9 +32,14 @@ QUnit.test('create (generated name)', makeServerTest(
   function(assert, server) {
     server.respondWith('POST', 'http://localhost:9998/applets', function(request) {
       var data = JSON.parse(request.requestBody);
-      assert.equal(data.code, 'my.sample.Applet', 'code parameter was set');
-      assert.ok(data.name == null, 'no name was specified');
-      request.respond(200);
+      
+      var applet = data.applet;
+      assert.ok(applet != null, 'applet info was sent');
+      assert.equal(applet.code, 'my.sample.Applet', 'code parameter was set');
+      assert.ok(applet.name == null, 'no name was specified');
+      
+      data.name = 'generated-name';
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(applet));
     });
   }
 ));
@@ -45,9 +58,13 @@ QUnit.test('create (specified name)', makeServerTest(
   function(assert, server) {
     server.respondWith('POST', 'http://localhost:9998/applets', function(request) {
       var data = JSON.parse(request.requestBody);
-      assert.equal(data.code, 'my.sample.Applet', 'code parameter was set');
-      assert.equal(data.name, 'test', 'the correct name was specified');
-      request.respond(200);
+      
+      var applet = data.applet;
+      assert.ok(applet != null, 'applet info was sent');
+      assert.equal(applet.code, 'my.sample.Applet', 'code parameter was set');
+      assert.equal(applet.name, 'test', 'the correct name was specified');
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(applet));
     });
   }
 ));
@@ -60,14 +77,16 @@ QUnit.test('check visibility', makeServerTest(
     }).then(function(applet) {
       return applet.visible();
     }).then(function(isVisible) {
+      assert.ok(isVisible === true, 'the visibility was received');
       testDone();
     });
   },
   
   function(assert, server) {
     server.respondWith('GET', 'http://localhost:9998/applets/test/visible', function(request) {
-      var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      var data = { visible: true };
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(data));
     });
   }
 ));
@@ -80,14 +99,17 @@ QUnit.test('get state', makeServerTest(
     }).then(function(applet) {
       return applet.state();
     }).then(function(state) {
+      assert.equal(state, 'STARTED', 'the state was received');
+      
       testDone();
     });
   },
   
   function(assert, server) {
     server.respondWith('GET', 'http://localhost:9998/applets/test/state', function(request) {
-      var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      var data = { state: 'STARTED' };
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(data));
     });
   }
 ));
@@ -99,7 +121,8 @@ QUnit.test('show', makeServerTest(
       name: 'test'
     }).then(function(applet) {
       return applet.show();
-    }).then(function() {
+    }).then(function(isVisible) {
+      assert.ok(isVisible === true, 'the new visibility was received');
       testDone();
     });
   },
@@ -107,7 +130,9 @@ QUnit.test('show', makeServerTest(
   function(assert, server) {
     server.respondWith('PUT', 'http://localhost:9998/applets/test/visible', function(request) {
       var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      assert.ok(data.visible === true, 'the correct visibility was sent');
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(data));
     });
   }
 ));
@@ -119,7 +144,8 @@ QUnit.test('hide', makeServerTest(
       name: 'test'
     }).then(function(applet) {
       return applet.hide();
-    }).then(function() {
+    }).then(function(isVisible) {
+      assert.ok(isVisible === true, 'the new visibility was received');
       testDone();
     });
   },
@@ -127,7 +153,9 @@ QUnit.test('hide', makeServerTest(
   function(assert, server) {
     server.respondWith('PUT', 'http://localhost:9998/applets/test/visible', function(request) {
       var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      assert.ok(data.visible === false, 'the correct visibility was sent');
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(data));
     });
   }
 ));
@@ -140,6 +168,9 @@ QUnit.test('call method', makeServerTest(
     }).then(function(applet) {
       return applet.callMethod('sum', 10, 5);
     }).then(function(resultOrError) {
+      assert.equal(resultOrError.result, 15, 'the method\'s result is correct');
+      assert.ok(resultOrError.error == null, 'no error was returned');
+      
       testDone();
     });
   },
@@ -147,7 +178,12 @@ QUnit.test('call method', makeServerTest(
   function(assert, server) {
     server.respondWith('POST', 'http://localhost:9998/applets/test/methods/sum', function(request) {
       var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      assert.equal(typeof data.args, 'array', 'the method\'s arguments were sent');
+      assert.equal(data.args.length, 2, 'all of the method\'s arguments were sent');
+      assert.equal(data.args[0], 10, 'the first argument is correct');
+      assert.equal(data.args[1], 5, 'the second argument is correct');
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify({ result: 15, error: null }));
     });
   }
 ));
@@ -159,7 +195,8 @@ QUnit.test('start stopped applet', makeServerTest(
       name: 'test'
     }).then(function(applet) {
       return applet.start();
-    }).then(function() {
+    }).then(function(state) {
+      assert.equal(state, 'STARTED', 'the new state was received');
       testDone();
     });
   },
@@ -167,7 +204,9 @@ QUnit.test('start stopped applet', makeServerTest(
   function(assert, server) {
     server.respondWith('PUT', 'http://localhost:9998/applets/test/state', function(request) {
       var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      assert.equal(data.state, 'STARTED', 'the correct state was sent');
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(data));
     });
   }
 ));
@@ -179,7 +218,8 @@ QUnit.test('stop started applet', makeServerTest(
       name: 'test'
     }).then(function(applet) {
       return applet.stop();
-    }).then(function() {
+    }).then(function(state) {
+      assert.equal(state, 'STOPPED', 'the new state was received');
       testDone();
     });
   },
@@ -187,7 +227,9 @@ QUnit.test('stop started applet', makeServerTest(
   function(assert, server) {
     server.respondWith('PUT', 'http://localhost:9998/applets/test/state', function(request) {
       var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      assert.equal(data.state, 'STOPPED', 'the correct state was sent');
+      
+      request.respond(200, RESPONSE_HEADERS, JSON.stringify(data));
     });
   }
 ));
@@ -207,7 +249,7 @@ QUnit.test('destroy applet', makeServerTest(
   function(assert, server) {
     server.respondWith('DELETE', 'http://localhost:9998/applets/test', function(request) {
       var data = JSON.parse(request.requestBody);
-      request.respond(200);
+      request.respond(204);
     });
   }
 ));
